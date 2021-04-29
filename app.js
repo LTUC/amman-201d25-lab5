d366ece5ff47f41be2d6b65135f77eb00f2c5fdd
sultan-elayan <sultan.m.elayan@gmail.com>
2021-04-29 13:30:09 +0300

    problem3

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function multiply(a, b) {
 
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
-console.log(testMultiply)
+console.log(testMultiply);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -53,12 +53,16 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+function sumAndMultiply(a, b, c) { 
+    let output1 = a + b + c ;
+    let output2 = a * b * c ;
+    return [output1 ,  output2 , `${a} and ${b} and ${c} sum to ${output1}.` , `The product of ${a} and ${b} and ${c} is ${output2}.` ];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
+console.log(testMultiply);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
