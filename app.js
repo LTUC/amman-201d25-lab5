9a2b8f58dc5f46b73a06bda200ea2564a7f1b7e5
wijdankhaled <wijdankhaled178@gmail.com>
2021-04-29 14:49:25 +0300

    problem3 is done

--- app.js
+++ app.js
@@ -29,11 +29,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+let output= a*b;
+return[output,`The product of ${a} and ${b} is ${output}.`];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,11 +51,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+let result1=sum(a,b)[0]
+let result2=sum(c,result1)[0]
+
+let output=multiply(a,b)[0]
+let output2=multiply(c,output)[0]
+return[result2,output2,`${a} and ${b} and ${c} sum to ${result2}.`,`The product of ${a} and ${b} and ${c} is ${output2}.`];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
