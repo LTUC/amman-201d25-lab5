df87269e43295861df4b7a6e0a0058dc23f151f9
Mahmoud-Barghouthi <mah.barghouthi47@gmail.com>
2021-04-29 16:29:09 +0300

    problem6

--- app.js
+++ app.js
@@ -50,12 +50,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-    let s1 = sum(a,b)[0];
-    let s2 = sum(s1,c)[0];
-    let m1 = multiply(a,b)[0];
-    let m2 = multiply(m1,c)[0];
+    let s1 = sum(a, b)[0];
+    let s2 = sum(s1, c)[0];
+    let m1 = multiply(a, b)[0];
+    let m2 = multiply(m1, c)[0];
 
-    return [s2, m2 ,`${a} and ${b} and ${c} sum to ${s2}.`,`The product of ${a} and ${b} and ${c} is ${m2}.`]
+    return [s2, m2, `${a} and ${b} and ${c} sum to ${s2}.`, `The product of ${a} and ${b} and ${c} is ${m2}.`]
 
 }
 
@@ -78,9 +78,9 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-  let sA = sum(sumArr[0], sum(sumArr[1],sumArr[2])[0])[0];
+    let sA = sum(sumArr[0], sum(sumArr[1], sumArr[2])[0])[0];
 
-  return [sA, `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sA} is their sum.`]
+    return [sA, `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sA} is their sum.`]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
@@ -103,9 +103,9 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 
 function multiplyArray(multArr) { //eslint-disable-line
-     let mA =  multiply(multArr[0], multiply(multArr[1],multArr[2])[0])[0];
+    let mA = multiply(multArr[0], multiply(multArr[1], multArr[2])[0])[0];
 
-     return [mA,`The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${mA}.`]
+    return [mA, `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${mA}.`]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
@@ -119,7 +119,9 @@ testMultiplyArray(testArray);
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose
+ first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments 
+ that were passed into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
@@ -130,13 +132,21 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
+let testDynamicArray = [1, 2, 3, 4, 5,]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+    let mL = 1;
 
+    for (let i = 0; i < dynamicArray.length; i++) {
+
+        mL = multiply(mL, dynamicArray[i])[0];
+
+    }
+    return [mL, `The numbers 1,2,3,4,5 have a product of 120.`]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
