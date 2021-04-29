c45d740af7c681135af32ef47e9a0f966a53dcf1
qusaiqeisi <qusaiqeisi6@gmail.com>
2021-04-29 15:42:38 +0300

    problem4

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function sum(a, b) { //eslint-disable-line
 let output= a+b;
 return[output,`The sum of ${a} and ${b} is ${output}.`]
 }
-console.log(sum);
+// console.log(sum);
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
@@ -34,7 +34,7 @@ function multiply(a, b) { //eslint-disable-line
 let output=a*b;
 return[output,`The product of ${a} and ${b} is ${output}.`]
 }
-console.log(multiply);
+// console.log(multiply);
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
 
@@ -57,7 +57,7 @@ let output1= a+b+c ;
 let output2 = a*b*c;
 return[output1,output2,`${a} and ${b} and ${c} sum to ${output1}.` ,`The product of ${a} and ${b} and ${c} is ${output2}.` ] 
 }
-console.log(sumAndMultiply);
+// console.log(sumAndMultiply);
 // Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4,7,5);
 
@@ -65,24 +65,34 @@ testSumAndMultiply(4,7,5);
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument and then 
+returns an array where the first element is the sum of the numbers in the array, and the second element 
+is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition,
+ use your sum() function that you've already created. 
+ You're going to have to be resourceful to figure out how to do this. 
+ However, you may continue to use the + operator for string concatenation.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished,
+ uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let output = sum(testArray[0] , testArray[1] )
+//   console.log(output);
+let output1 = sum (output[0] , testArray[2])
+// console.log (output1);
+return[output1[0] ,`${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${output1[0]} is their sum.` ]
 }
-
+// sumArray();
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
