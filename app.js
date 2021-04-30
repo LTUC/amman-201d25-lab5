e4a06b2af5e88ee31aa074112b740387a851f2d7
muntherabdlrahman <munther.abdlrahman@gmail.com>
2021-04-30 23:55:31 +0300

    fixed the lab problem 3

--- app.js
+++ app.js
@@ -66,14 +66,20 @@ Test this function by hand in the console to get it working,
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+let resl=sum(a,b)[0];
+let reslt0=sum(resl,c)[0];
 
+let reslPro=multiply(a,b)[0];
+let resltpro2=multiply(reslPro,c)[0];
+
+return[reslt0, resltpro2, `${a} and ${b} and ${c} sum to ${reslt0}.`, `The product of ${a} and ${b} and ${c} is ${resltpro2}.`]
 
 }
 // Here is the test for sumAndMultiply(); uncomment it to run it
 
 //testSumAndMultiply(4,7,5);
 
-//testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
