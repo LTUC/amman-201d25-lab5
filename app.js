dbcf2782504dc57c456478468547a61bbf0f792f
MaryamAlEisah <maryam.aleisah@gmail.com>
2021-04-30 12:08:45 +0300

    problem4 done

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ let sumAndMultiply= [r, s, thirdElement,fourthElement]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-//testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -82,15 +82,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
+var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  
+  var x =sum(testArray[0],testArray[1])[0];
+  var sumArr = sum(x,testArray[2])[0];
+  var sumArray  = [sumArr,testArray[0]+','+testArray[1]+','+testArray[2]+' was passed in as an array of numbers, and '+sumArr+' is their sum.'];
+ return sumArray;
 }
+
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
